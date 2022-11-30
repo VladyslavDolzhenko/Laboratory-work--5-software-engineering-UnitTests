c4c1177a9a655fcbfa8d1ec9575039f5fa3ea88a
VladyslavDolzhenko <dolzenkovlad@gmail.com>
2022-11-30 11:31:37 +0100

    Init commit

---
+++ Lab2UnitTests.cpp
@@ -0,0 +1,146 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:\Projects\Laboratory work  №2(software engineering)\Laboratory work  №2(software engineering)\Laboratory work  №2(software engineering).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab2UnitTests
+{
+	TEST_CLASS(checkValidParams_Tests)
+	{
+	public:
+		TEST_METHOD(checkValidParams_get0and1and3and1_exceptionThrown)
+		{
+			int n = 0;
+			double a = 1;
+			double b = 3;
+			double h = 1;
+
+			try {
+				checkValidParams(n, a, b, h);
+				Assert::Fail();
+			}
+			catch (...) {
+				Assert::IsTrue(true);
+			}
+		}
+
+	public:
+		TEST_METHOD(checkValidParams_get1and4and3and1_exceptionThrown)
+		{
+			int n = 1;
+			double a = 4;
+			double b = 3;
+			double h = 1;
+
+			try {
+				checkValidParams(n, a, b, h);
+				Assert::Fail();
+			}
+			catch (...) {
+				Assert::IsTrue(true);
+			}
+		}
+	
+	public:
+		TEST_METHOD(checkValidParams_get1and5and9and2_exceptionThrown)
+		{
+			int n = 1;
+			double a = 5;
+			double b = 9;
+			double h = -2;
+
+			try {
+				checkValidParams(n, a, b, h);
+				Assert::Fail();
+			}
+			catch (...) {
+				Assert::IsTrue(true);
+			}
+		}
+			
+	public:
+		TEST_METHOD(checkValidParams_get8and3and15and2_exceptionThrown)
+		{
+			int n = 8;
+			double a = 3;
+			double b = 15;
+			double h = 2;
+
+			try {
+				checkValidParams(n, a, b, h);
+				Assert::IsTrue(true);
+			}
+			catch (...) {
+				Assert::Fail();
+			}
+		}
+
+	public:
+		TEST_METHOD(checkValidParams_get0and6and3and0_exceptionThrown)
+		{
+			int n = 0;
+			double a = 6;
+			double b = 3;
+			double h = 0;
+
+			try {
+				checkValidParams(n, a, b, h);
+				Assert::Fail();
+			}
+			catch (...) {
+				Assert::IsTrue(true);
+			}
+		}
+	};
+
+	TEST_CLASS(checkValidInput_Tests)
+	{
+	public:
+		TEST_METHOD(checkValidInput_exceptionThrown)
+		{
+			try {
+				checkValidInput();
+				Assert::IsTrue(true);
+			}
+			catch (...) {
+				Assert::Fail();
+			}
+		}
+	};
+
+	TEST_CLASS(calculate_Tests)
+	{
+	public:
+		TEST_METHOD(calculate_get3and9and1and5_truereturned)
+		{
+			double a = 3;
+			double b = 9;
+			double h = 1;
+			int n = 5;
+			int expected = true;
+
+			int actual = calculate(a, b, h, n);
+
+			Assert::AreEqual(expected, actual);
+		}
+
+	public:
+		TEST_METHOD(calculate_get3and9and1and1_exceptionThrown)
+		{
+			double a = -3;
+			double b = 9;
+			double h = 1;
+			int n = 1;
+
+			try {
+				calculate(a, b, h, n);
+				Assert::Fail();
+			}
+			catch (...) 
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+}
---
+++ Laboratory work  №2(software engineering).cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <fstream>
+#include <math.h>
+using namespace std;
+
+void checkValidInput() {
+	if (cin.fail()) {
+		throw "Incorrect input!";
+	}
+}
+
+void checkValidParams(int n, double a, double b, double h) {
+	if (n <= 0) {
+		throw "Error, change your n!";
+	} else if (n > 0) {
+		if (a > b) {
+			throw "Your a > b!";
+		} else if (a <= b) {
+			if (h <= 0) {
+				throw "Your h <= 0";
+			}
+		}
+	}
+}
+
+int choice;
+
+bool calculate(double a, double b, double h, int n) {
+
+	for (double x = a; x <= b; x += h) {
+		if (x >= 0) {
+			int i = 0;
+			double y = 0;
+			if (n == 1) {
+				y = x - 1;
+				cout << "x = " << x << "; y = " << y << endl;
+				if (choice == 1) {
+					ofstream fout("results.txt", ios_base::app);
+					fout << "x = " << x << "; y = " << y << endl;
+				}
+			} else if (n != 1) {
+				y = 0;
+				int k = n - 1;
+
+				for (i = 0; i <= k; i++) {
+					y += (x - 1) / (i + 1);
+				}
+				cout << "x = " << x << "; y = " << y << endl;
+				if (choice == 1) {
+					ofstream fout("results.txt", ios_base::app);
+					fout << "x = " << x << "; y = " << y << endl;
+				}
+			}
+		} else if (x < 0) {
+			if (n == 1) {
+				throw "Error, change your n!";
+			} else if (n > 1) {
+				double y = 0;
+				int k = n - 1;
+
+				for (int i = 1; i <= k; i++) {
+					double sum = 0;
+
+					for (int j = 1; j <= n; j++) {
+						sum += x - pow(i, 2) + j;
+					}
+					y += sum;
+				}
+				cout << "x = " << x << "; y = " << y << endl;
+				if (choice == 1) {
+					ofstream fout("results.txt", ios_base::app);
+					fout << "x = " << x << "; y = " << y << endl;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+
+	int main() {
+		int n;
+		double a, b, h;
+		double y = 0;
+
+		try {
+			cout << "Enter a (from), b (to), h (step) and n: ";
+			cin >> a >> b >> h >> n;
+			checkValidInput();
+			checkValidParams(n, a, b, h);
+			choice;
+			cout << "Do you want to record the results of each step written to a file?\nYes - 1; No - 0" << endl;
+			cin >> choice;
+			checkValidInput();
+			calculate(a, b, h, n);
+		}
+		catch (const char* ex) {
+			cout << ex << endl;
+			return -1;
+		}
+		catch (...) {
+			cout << "Unknown error!" << endl;
+			return -2;
+		}
+		return 0;
+	}
